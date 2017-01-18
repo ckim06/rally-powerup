0a218f5d6b8d40c817a6625d5e97dcea20a604a8
Kim, Christopher (CORP) <Chris.S.Kim@adp.com>
2017-01-18 13:18:36 -0800

    added est hours

--- web/template.js
+++ web/template.js
@@ -16,7 +16,7 @@ TrelloPowerUp.initialize({
       return [{
         id: 'rally',
         icon: GRAY_ICON,
-        title: 'Tasks',
+        title: 'Rally Tasks',
         claimed:[
           'https://power-up-trello.herokuapp.com/web/index.html'
         ],
